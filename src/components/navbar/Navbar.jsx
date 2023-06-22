7dbabdd7a9c778c3e8760ae20dc5ee55417f33e4
Eric Mwangi <113993015+MustangSn@users.noreply.github.com>
2023-06-22 15:59:32 +0300

    Navbar styling fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,43 +1,60 @@
-import React from "react";
-import styles from "./navbar.module.css";
+import React from "react"
 import { Link } from "react-router-dom";
-const links = [
-  { title: "About", url: "/about", id: 1 },
-  { title: "Services", url: "/terms-of-use", id: 2 },
-  { title: "Investment", url: "/investment", id: 3 },
-  { title: "Property", url: "/property", id: 4 },
-  { title: "Resources", url: "/resources", id: 5 },
-  { title: "Dashboard", url: "/dashboard", id: 6 },
-];
 
-const button = (bg, color, border) => {};
-const Navbar = () => {
-  return (
-    <div className={styles.container}>
-      <Link to="/" className={styles.logo}>
-        <img
-          src={require("../Dashboard/img/logo2.png")}
-          style={{ width: "144px" }}
-          alt=""
-        />
-      </Link>
-      <div className={styles.linkCont}>
-        {links.map((link) => (
-          <Link to={link.url} key={link.id} className={styles.links}>
-            {link.title}
-          </Link>
-        ))}
-      </div>
-      <div className={styles.btnCont}>
-        <Link href="/login">
-          <button className={`${styles.btn} ${styles.secondary}`}>Login</button>
-        </Link>
-        <Link href="register">
-          <button className={`${styles.btn} ${styles.primary}`}>Sign Up</button>
-        </Link>
-      </div>
-    </div>
-  );
-};
 
-export default Navbar;
+export default function Navbar() {
+    return (
+        <div className="container-fluid">
+            <div className="navbar navbar-expand-sm bg-white">
+                <div className="container-fluid pt-5">
+
+                    <div class="container-fluid">
+
+
+                        <Link className="navbar-brand" to='/'>
+                            <img src={require('../Dashboard/img/logo2.png')} style={{ width: '144px' }} alt="" />
+                        </Link>
+
+
+                    </div>
+
+                    <ul class="navbar-nav">
+                        <div className="navbar-nav">
+                            <li class="nav-item">
+                                <Link class="nav-link px-4" id="common_txt" to="#"><h4>About</h4></Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link px-4" id="common_txt" to="#"><h4>Services</h4></Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link px-4" id="common_txt" to={`/investment`}><h4>Investments</h4></Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link px-4" id="common_txt" to="#"><h4>Property</h4></Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link px-4" id="common_txt" to="/dashboard"><h4>Dashboard</h4></Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link px-4" id="common_txt" to="#"><h4>Resources</h4></Link>
+                            </li>
+                        </div>
+                        <li class="nav-item">
+                            <Link class="nav-link" to="/login">
+                                <button className="btn btn-light btn-outline-secondary text-nowrap px-4 py-0 border border-1 border-dark" id="common_txt"><h4 className="px-5">Log In</h4></button>
+                            </Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link" to="/signup">
+                                <button className="btn btn-primary text-nowrap px-4 py-0" id="btn_signup"><h4 className="px-5">Sign Up</h4></button>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div >
+
+
+
+        </div >
+    );
+}
