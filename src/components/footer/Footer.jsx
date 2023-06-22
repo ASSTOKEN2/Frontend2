e9e86a5edc7d265e22b32f87b35e56b10b7d0702
Eric Mwangi <113993015+MustangSn@users.noreply.github.com>
2023-06-22 15:43:25 +0300

    Footer styling fixed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,67 +1,52 @@
-"use client";
-import React from "react";
-import styles from "./footer.module.css";
-import {
-  BsSend,
-  BsLinkedin,
-  BsFacebook,
-  BsInstagram,
-  BsTwitter,
-  BsYoutube,
-} from "react-icons/bs";
-
-const Footer = () => {
+export default function Footer() {
   return (
-    <div className={styles.container}>
-      <div className={styles.contactInfo}>
-        <div className={styles.firstContainer}>
-          <h2 className={styles.logo}>LOGO</h2>
-          <p className={styles.logoText}></p>
-          At AssetToken our primary aim is to help land owners and property
-          managers to connect to the right audience of investors while also
-          allowing interested investors to share their purchasing power and
-          invest in their so desired properties without the fear of being unable
-          to afford this properties. Through the use of blockchain technology.
+    <div className="container-fluid mt-5" id="common_bg">
+      <div className="mt-5" >
+        <div className="row text-white p-5">
+          <div className="col-sm-4 p-5">
+            <a href="" className="text-white">LOGO</a>
+            <p className="pt-5">At AssetToken our primary aim is to help land owners and property managers to connect to the right audience of investors while also allowing interested investors to share their purchasing power and invest in their so desired properties without the fear of being unable to afford this properties. Through the use of blockchain technology.</p>
+
+          </div>
+          <div className="col-sm-1"></div>
+          <div className="col-sm-2 p-5 pt-3 px-0">
+            <h3>Features</h3>
+            <a className="nav-link p-1" href="#">Services</a>
+            <a className="nav-link p-1" href="#">Investment</a>
+            <a className="nav-link p-1" href="#">Property</a>
+            <a className="nav-link p-1" href="#">Resources</a>
+          </div>
+          <div className="col-sm-3 p-5 pt-3 px-0">
+            <h3>More Information</h3>
+            <a className="nav-link p-1" href="#">Contact us</a>
+            <a className="nav-link p-1" href={`/PrivacyPolicy`}>Privacy Policy</a>
+            <a className="nav-link p-1" href={`/Blog`}>FAQs</a>
+            <a className="nav-link p-1" href={`/TermsOfUse`}>Terms of Use</a>
+            <a className="nav-link p-1" href={`/PrivacyPolicy#Disclaimer`}>Disclaimer</a>
+          </div>
+          <div className="col-sm-2 p-5 pt-3 px-0">
+            <h3>Contact Us</h3>
+          </div>
+          <div className="clearfix mb-5">
+            <div className="ps-5 float-start">
+              <a className="p-3 ps-0" href=""><img className="" width="40" src="/Images/Discord.png" /></a>
+              <a className="p-3" href=""><img className="" width="40" src="/Images/IG.png" /></a>
+              <a className="p-3" href=""><img className="" width="40" src="/Images/Linkedin.png" /></a>
+            </div>
+            <div className="float-end pe-5 bg-white rounded-3 mb-5">
+              <button className="bg-transparent border-0">
+                <input type="text" value="Hello How May I Help You Today" className="border-0 py-5 ps-4 rounded-3" id="bot">
+                </input>
+
+                <img src="Images/send.png" width="60" className="pe-5" />
+              </button>
+            </div>
+          </div>
+          <p><span className="glyphicon glyphicon-copyright-mark"></span> 2023 <a href={`/`} className="text-white">AssetToken.com</a>. All Right Reserved</p>
         </div>
-        <div className={styles.secondContainer}>
-          <h2 className={styles.header}>Features</h2>
-          <ul className={styles.list}>
-            <li className={styles.listItem}>services</li>
-            <li className={styles.listItem}>Investment</li>
-            <li className={styles.listItem}>Property</li>
-            <li className={styles.listItem}>Resources</li>
-          </ul>
-        </div>
-        <div className={styles.thirdContainer}>
-          <h2 className={styles.header}>Other Informations</h2>
-          <ul className={styles.list}>
-            <li className={styles.listItem}>Contact Us</li>
-            <li className={styles.listItem}>Privacy Policy</li>
-            <li className={styles.listItem}>FAQs</li>
-            <li className={styles.listItem}>Term Of Use</li>
-          </ul>
-        </div>
-        <div className={styles.fourthContainer}>
-          <h2 className={styles.header}>Contact Us</h2>
-        </div>
-      </div>
-      <div className={styles.signUp}>
-        <div className={styles.media}>
-          <BsInstagram />
-          <BsLinkedin />
-          <BsTwitter />
-        </div>
-        <div className={styles.inputContainer}>
-          <input
-            type="text"
-            placeholder="Hello How May i Help You Today"
-            className={styles.input}
-          />
-          <BsSend style={{ zIndex: "11111", color: "#0066F5" }} />
-        </div>
+
+
       </div>
     </div>
-  );
-};
-
-export default Footer;
+  )
+}
