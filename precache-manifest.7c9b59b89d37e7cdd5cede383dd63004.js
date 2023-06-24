afbba3a5ba4aa9adc53766e86c19258c609f8bfc
pablo-codes <112553944+pablo-codes@users.noreply.github.com>
2023-06-24 17:47:03 +0100

    Updates

---
+++ precache-manifest.7c9b59b89d37e7cdd5cede383dd63004.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "24e44700aeea06b88af4",
+    "url": "/Frontend2/static/css/main.64f02ab0.chunk.css"
+  },
+  {
+    "revision": "24e44700aeea06b88af4",
+    "url": "/Frontend2/static/js/main.24e44700.chunk.js"
+  },
+  {
+    "revision": "fe817d6134e0607d997d",
+    "url": "/Frontend2/static/js/1.fe817d61.chunk.js"
+  },
+  {
+    "revision": "aa4ec221a07395e69b9b",
+    "url": "/Frontend2/static/css/2.6f1b6e41.chunk.css"
+  },
+  {
+    "revision": "aa4ec221a07395e69b9b",
+    "url": "/Frontend2/static/js/2.aa4ec221.chunk.js"
+  },
+  {
+    "revision": "91da376225232cc6c3b8",
+    "url": "/Frontend2/static/js/runtime~main.91da3762.js"
+  },
+  {
+    "revision": "74db16ac283fc3c944c8d7dd45b67e10",
+    "url": "/Frontend2/static/media/logo.74db16ac.png"
+  },
+  {
+    "revision": "29326cce90d4b2f5447cbd8640d87977",
+    "url": "/Frontend2/static/media/policyone.29326cce.png"
+  },
+  {
+    "revision": "3d1ef890c598d4042f1686c6c25f927c",
+    "url": "/Frontend2/static/media/documenting.3d1ef890.png"
+  },
+  {
+    "revision": "877f7f594599d3d73df0dfe42d9ab7e6",
+    "url": "/Frontend2/static/media/tracking.877f7f59.png"
+  },
+  {
+    "revision": "dfedfe2b20807a7206a0c5beedcbbe73",
+    "url": "/Frontend2/static/media/lookingfuture.dfedfe2b.png"
+  },
+  {
+    "revision": "4d2ddce598fd4bcf83dd1c7028bc0567",
+    "url": "/Frontend2/static/media/snoowhouse.4d2ddce5.png"
+  },
+  {
+    "revision": "b15e7e37d9bd806835e12ae535df3d4f",
+    "url": "/Frontend2/static/media/icon_search.b15e7e37.svg"
+  },
+  {
+    "revision": "9aea806dbf838f574307a1cdaa30d468",
+    "url": "/Frontend2/static/media/bell.9aea806d.svg"
+  },
+  {
+    "revision": "d74b10fc0e0db96ad6446a2e65dc35b7",
+    "url": "/Frontend2/static/media/msg.d74b10fc.svg"
+  },
+  {
+    "revision": "2859f3d642f77ce700fb850fb684c49f",
+    "url": "/Frontend2/index.html"
+  }
+];
